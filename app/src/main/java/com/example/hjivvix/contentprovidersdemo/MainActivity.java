a83cb28cf2e93abeb939d51d684e057c23807f3e
haranath <haranathchowdary89@gmail.com>
2018-10-04 21:14:57 +0530

    Added Button

--- app/src/main/java/com/example/hjivvix/contentprovidersdemo/MainActivity.java
+++ app/src/main/java/com/example/hjivvix/contentprovidersdemo/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.hjivvix.contentprovidersdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
     }
 }
